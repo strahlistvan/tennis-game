fa2d6ba928f5f8543d65c648b34b288cd249b575
strahlistvan <strahlistvan@users.noreply.github.com>
2016-08-16 23:45:21 +0200

    Put Racket functions to new file

---
+++ Racket.cpp
@@ -0,0 +1,77 @@
+#include <SDL/SDL.h>
+#include <inttypes.h>
+#include "Racket.h"
+#define STEP_SIZE 4
+
+Racket::Racket()
+{
+	SDL_Surface * screen = SDL_GetVideoSurface();
+	racket_rect.h = 20; 	//racket height
+	racket_rect.w = 60;   //racket width
+	racket_rect.x = (screen->w)/2 -(racket_rect.w)/2;
+	racket_rect.y = (screen->h) - racket_rect.h;
+}
+
+Racket::Racket(int pos_x, int pos_y)
+{
+	racket_rect.h = 20; 	//racket height
+	racket_rect.w = 60;   //racket width
+	racket_rect.x = pos_x -(racket_rect.w)/2;
+	racket_rect.y = pos_y - racket_rect.h;
+}
+
+void Racket::moveRight()
+{
+	SDL_Surface * screen = SDL_GetVideoSurface();
+	if (racket_rect.x < (screen->w) - racket_rect.w) 
+		racket_rect.x += STEP_SIZE;
+}		
+
+void Racket::moveLeft()
+{
+	if (racket_rect.x > 0)
+		racket_rect.x -= STEP_SIZE;
+}
+		
+int Racket::getX()
+{
+	return racket_rect.x;
+}
+		
+int Racket::getY()
+{
+	return racket_rect.y;
+}
+			
+int Racket::getWidth()
+{
+	return racket_rect.w;
+}
+
+		
+int Racket::getHeight()
+{
+	return racket_rect.h;
+}
+		
+void Racket::addX(int _x)
+{
+	racket_rect.x += _x;
+}
+		
+void Racket::addY(int _y)
+{
+	racket_rect.y += _y;
+}
+		
+SDL_Rect Racket::getRect()
+{
+	return this->racket_rect;
+}
+
+void Racket::paint()
+{
+	SDL_Surface * screen = SDL_GetVideoSurface(); 
+	u_int32_t color = SDL_MapRGB(screen->format, 200, 0, 200);
+	SDL_FillRect(screen, &racket_rect, color);	
+}
--- Racket.h
+++ Racket.h
@@ -1,6 +1,4 @@
 #include <SDL/SDL.h>
-#include <inttypes.h>
-#define STEP_SIZE 4
 
 class Racket 
 {
@@ -8,77 +6,20 @@ class Racket
 		SDL_Rect racket_rect;
 		
 	public:
-		Racket()
-		{
-			SDL_Surface * screen = SDL_GetVideoSurface();
-			racket_rect.h = 20; 	//racket height
-			racket_rect.w = 60;   //racket width
-			racket_rect.x = (screen->w)/2 -(racket_rect.w)/2;
-			racket_rect.y = (screen->h) - racket_rect.h;
-		}
+		Racket();
+		Racket(int pos_x, int pos_y);
 		
-		Racket(int pos_x, int pos_y)
-		{
-			racket_rect.h = 20; 	//racket height
-			racket_rect.w = 60;   //racket width
-			racket_rect.x = pos_x -(racket_rect.w)/2;
-			racket_rect.y = pos_y - racket_rect.h;
-		}
+		void moveRight();
+		void moveLeft();
 		
-		void moveRight()
-		{
-			SDL_Surface * screen = SDL_GetVideoSurface();
-			if (racket_rect.x < (screen->w) - racket_rect.w) 
-				racket_rect.x += STEP_SIZE;
-		}
+		int getX();
+		int getY();
+		int getWidth();		
+		int getHeight();
+		void addX(int _x);	
+		void addY(int _y);
+		SDL_Rect getRect();
 		
-		void moveLeft()
-		{
-			if (racket_rect.x > 0)
-				racket_rect.x -= STEP_SIZE;
-		}
-		
-		int getX()
-		{
-			return racket_rect.x;
-		}
-		
-		int getY()
-		{
-			return racket_rect.y;
-		}
-		
-		int getWidth()
-		{
-			return racket_rect.w;
-		}
-		
-		int getHeight()
-		{
-			return racket_rect.h;
-		}
-		
-		void addX(int _x)
-		{
-			racket_rect.x += _x;
-		}
-		
-		void addY(int _y)
-		{
-			racket_rect.y += _y;
-		}
-		
-		SDL_Rect getRect()
-		{
-			return this->racket_rect;
-		}
-		
-		void paint()
-		{
-		   SDL_Surface * screen = SDL_GetVideoSurface(); 
-		   u_int32_t color = SDL_MapRGB(screen->format, 200, 0, 200);
-		   SDL_FillRect(screen, &racket_rect, color);	
-		}
-		
+		void paint();
 		
 };
